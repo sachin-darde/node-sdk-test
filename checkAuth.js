0457f430f106b0cb3a80c2b52967d77266f82dce
Sachin Darde <sachindarde@MacBook-Pro.local>
2023-06-23 19:19:18 +0530

    Fixed issue in check-auth

--- checkAuth.js
+++ checkAuth.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function checkAuth(apiBaseUrl, uid, permission) {
   try {
-    const response = await axios.get(`${apiBaseUrl}/check-auth`, {
+    const response = await axios.post(`${apiBaseUrl}/check-auth`, {
       data: JSON.stringify({
         "uid": uid,
         "permission": permission
