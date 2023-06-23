291c76fcdd00ca3ade35d040899541d7132fefac
sachin darde <sachin@dreamkasper.com>
2023-06-23 19:35:18 +0530

    Fixed header issue

--- checkAuth.js
+++ checkAuth.js
@@ -3,6 +3,9 @@ const axios = require('axios');
 async function checkAuth(apiBaseUrl, uid, permission) {
   try {
     const response = await axios.post(`${apiBaseUrl}/check-auth`, {
+      headers: { 
+        'Content-Type': 'application/json'
+      },
       data: JSON.stringify({
         "uid": uid,
         "permission": permission
