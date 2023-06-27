27a469d441682842e0aefa1cb954998dca6d232f
sachin darde <sachin@dreamkasper.com>
2023-06-27 13:21:51 +0530

    Fixed check auth

--- src/check-auth.js
+++ src/check-auth.js
@@ -1,29 +1,46 @@
-const axios = require('axios').default;
+const https = require('https');
 
 async function checkAuth(apiBaseUrl, uid, permission) {
-  try {
-      var data = JSON.stringify({
-          "uid": uid,
-          "permission": permission
+  return new Promise((resolve, reject) => {
+    const data = JSON.stringify({
+      uid: uid,
+      permission: permission
+    });
+
+    const options = {
+      hostname: apiBaseUrl,
+      path: '/v1/auth/check-auth',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(data)
+      }
+    };
+
+    const req = https.request(options, (res) => {
+      let responseData = '';
+
+      res.on('data', (chunk) => {
+        responseData += chunk;
       });
-      var config = {
-          method: 'post',
-          url: `${apiBaseUrl}/check-auth`,
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          data: data
-      };
 
-      const response = await axios(config)
-      return response.data.data;
-  } catch (error) {
-      if (error && error.response && error.response.data && error.response.data.message)
-          throw error.response.data.message;
-      else
-          throw error;
-  }
+      res.on('end', () => {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          const response = JSON.parse(responseData);
+          resolve(response.data);
+        } else {
+          reject(new Error(`Request failed with status code ${res.statusCode}`));
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    req.write(data);
+    req.end();
+  });
 }
-
 
 module.exports = checkAuth;
